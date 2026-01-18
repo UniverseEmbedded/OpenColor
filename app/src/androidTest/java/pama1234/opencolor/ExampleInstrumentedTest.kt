505b601a72dc5ee98036fe839bb8e0fee3da4eef
pama1234 <pama1234@163.com>
2026-01-18 10:02:27 +0800

    init

---
+++ app/src/androidTest/java/pama1234/opencolor/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package pama1234.opencolor
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+  @Test
+  fun useAppContext() {
+    // Context of the app under test.
+    val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+    assertEquals("pama1234.opencolor", appContext.packageName)
+  }
+}
